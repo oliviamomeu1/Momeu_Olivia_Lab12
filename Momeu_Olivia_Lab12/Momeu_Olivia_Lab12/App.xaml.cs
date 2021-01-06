4d6d31382927a97491a49bb7f0a3ce45acaf41fb
OLIVIA MARIA MOMEU <momeu.olivia165347@student.ubbcluj.ro>
2021-01-06 14:34:08 +0200

    Add project files.

---
+++ Momeu_Olivia_Lab12/Momeu_Olivia_Lab12/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Momeu_Olivia_Lab12.Data;
+
+namespace Momeu_Olivia_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
